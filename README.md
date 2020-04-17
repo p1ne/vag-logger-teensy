22b19b251bca315bcaaf5a889b840bcf44626080
p1ne <8995611+p1ne@users.noreply.github.com>
2020-04-17 16:24:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# can-dumper-teensy
+
+This project is intended to dump selected CAN bus messages to SD card using Teensy 3.6 board.
+
+This particular code is designed for Audi (A4B8/Q5 platform) to track RPM, cylinder misfires and cylinder ignition retardation angles. Misfires generate beeps using beeper wired to Analog output of Arduino.
+
+## Libraries
+* https://github.com/pawelsky/FlexCAN_Library - this one has timeouts for RX messages
+* https://github.com/PaulStoffregen/SD
+
+## Hardware used
+
+* Teensy 3.6
+* CAN differential to serial converter
+* 5V voltage regulator
+* OBD2 male and female connectors wired as pass-through
+
+## Wiring
+
+CAN0 alternative pins of Teensy are soldered to TXD and RXD pins of CAN converter
+3.3V and GND pins of converter are soldered to respective voltage and GND pins of Teensy
+CANH and CANL pins of CAN converter are soldered to pins 14 and 6 of OBD respectively
+
+Beeper is wired to A2 and GND pins of Teensy
+
+## CAN messages
+
+Response messages:
+
+All messages are 8 bytes long
+
+7E8 05 62 20 0x6F rpm1 rpm2 - RPM message where RPM value is rpm1*256+rpm2
+
+7E8 05 62 20 cyl ret1 ret2 - Retardation angles message, where cyl is 0A-0D for cylinders 1-4, and ret1*256+ret2 is signed int negative HEX value of angle
+
+7E8 05 62 29 cyl misf1 misf2 - Misfire for last 1000 RPM message, where cyl is 1D-20 for cylinders 1-4 and misf1*256+misf2 is number of misfires for last 1000 RPM.
+
+Request messages are the same, but with 7E0 instead of 7E8 and 0x55 in last 4 bytes. Message length is 8 bytes.
+
+... the rest of messages are in the code ..
+
+## Work logic
+
+all messages but 0x7E8 are filtered by CAN masks/filters to lower CAN traffic 
+
+messages are sent sequentally with pause of 120 msec inbetween
+
+listener of messages is listening for response message within 30 msec
+
+as there are unpredicted responses from another CAN tool I have plugged constantly in the car - CAN Sensor, messages are processed until listening queue is empty
+
+0 misfires and 0 retards are not logged
+
+MPI and FSI injectors activity is logged together with RPM in each line - and identified by the number of respective injections increasing from last time.
+
+
+Logic to be fully implemented (as I had in can-dumper-sd project)
+For misfires only 1st misfire is saved. If more misfires are coming within 1000 RPM - they are saved too. But in order to save log file size, we don't save subsequent messages with the same misfire value. Upon receiption of misfire message 1000 Hz 500msec long beep is generated through beeper
+
---
+++ can-dumper-teensy/can-dumper-teensy.ino
@@ -0,0 +1,214 @@
+#include "FlexCAN.h"
+#include "SD.h"
+#include <Metro.h>
+
+Metro serialMetro = Metro(120);
+
+FlexCAN Can0(500000,0,1,1);
+
+String str;
+File dataFile;
+
+uint32_t time;
+
+uint32_t lastDirectInjections = 0;
+uint32_t lastMPIInjections = 0;
+String currentInjectors = "";
+
+uint16_t lastRPM = 0;
+
+CAN_message_t msg;
+CAN_message_t txmsg[20];
+
+CAN_filter_t defaultMask;
+CAN_filter_t defaultFilter;
+
+uint8_t messagesNum = 0;
+uint8_t currentMessage = 0;
+
+void can_msg_set(uint8_t idx, uint32_t id, uint8_t len, uint8_t buf_0, uint8_t buf_1, uint8_t buf_2, uint8_t buf_3, uint8_t buf_4, uint8_t buf_5, uint8_t buf_6, uint8_t buf_7)
+{
+  txmsg[idx].id = id;
+  txmsg[idx].len = len;
+  txmsg[idx].buf[0] = buf_0;
+  txmsg[idx].buf[1] = buf_1;
+  txmsg[idx].buf[2] = buf_2;
+  txmsg[idx].buf[3] = buf_3;
+  txmsg[idx].buf[4] = buf_4;
+  txmsg[idx].buf[5] = buf_5;
+  txmsg[idx].buf[6] = buf_6;
+  txmsg[idx].buf[7] = buf_7;
+}
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  Serial.begin(115200);
+  delay(2000);
+
+  defaultMask.id = 0x7FF;
+
+  defaultFilter.id = 0x7E8;
+
+  pinMode(A2, OUTPUT);
+
+  Serial.println("Init");
+
+  Can0.begin();
+  for (int i=0;i<8;i++) {
+    Can0.setFilter(defaultFilter,i);
+  }
+
+  if (!SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+
+  Serial.println("card initialized.");
+  dataFile = SD.open("can-bus.txt", FILE_WRITE);
+  if (dataFile) {            
+    dataFile.println("---");
+    dataFile.flush();
+    Serial.println("New session started");
+  }
+
+  str = "";
+
+  // no retard messages as they're sent by CAN sensor
+  
+  // RPM
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x20, 0x6F, 0x55, 0x55, 0x55, 0x55 );
+  // misfires
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x1D, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x1E, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x1F, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x20, 0x55, 0x55, 0x55, 0x55 );
+  // number of direct injections
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x42, 0x67, 0x55, 0x55, 0x55, 0x55 );
+  // number of MPI injections
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x42, 0x68, 0x55, 0x55, 0x55, 0x55 );
+  // fuel low: actual
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x20, 0x25, 0x55, 0x55, 0x55, 0x55 );
+  // fuel low: specified
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x32, 0x55, 0x55, 0x55, 0x55 );
+  // fuel high: actual
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x20, 0x27, 0x55, 0x55, 0x55, 0x55 );
+  // fuel high: specified
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x29, 0x3B, 0x55, 0x55, 0x55, 0x55 );
+  // knock sensor voltages
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x39, 0x60, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x39, 0x61, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x39, 0x62, 0x55, 0x55, 0x55, 0x55 );
+  can_msg_set(messagesNum++, 0x7E0, 8, 0x03, 0x22, 0x39, 0x63, 0x55, 0x55, 0x55, 0x55 );
+}
+
+void loop()
+{
+ if (serialMetro.check() == 1) {
+    //Serial.println(">>> " + String(millis()) + " " + String(currentMessage));
+    Can0.write(txmsg[currentMessage]);
+    currentMessage++;
+    if (currentMessage == messagesNum) currentMessage = 0;
+  
+    msg.timeout = 30;
+
+    while (Can0.read(msg)) {
+      time = millis();
+      String str = "";
+      
+      //Serial.print(String(time) + ": " + String(msg.id, HEX) + " " + String(msg.len) + " ");
+      //for (int i=0;i<8;i++)
+      //  Serial.print(String(msg.buf[i], HEX) + " ");
+      //Serial.println();
+      
+      // current RPM
+      if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x20) && (msg.buf[3] == 0x6F)) {
+        lastRPM = msg.buf[4]*256+msg.buf[5];
+      }
+
+      // retards
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x20) && (msg.buf[3] >= 0x0A) && (msg.buf[3] <= 0x0D)) {
+        int canValue = msg.buf[4]*256+msg.buf[5];
+        if (canValue != 0) {
+          str = str + "ret \t" + String(msg.buf[3]-0x9) + "\t" + String((float)(signed int)(canValue) / 100.0);;
+        }
+      }
+      
+      // misfires
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x29) && (msg.buf[3] >= 0x1D) && (msg.buf[3] <= 0x20)) {
+        int misfires = msg.buf[4]*256+msg.buf[5];
+        if (misfires > 0) {
+          str = str + "misfire \t" + String(msg.buf[3]-0x1C) + "\t" + String(misfires);
+          tone(A2,1000,500);
+        }
+      }
+
+      // knock voltage
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x39) && (msg.buf[3] >= 0x60) && (msg.buf[3] <= 0x63)) {
+        int knockV = msg.buf[4]*256+msg.buf[5];
+        str = str + "knock V \t" + String(msg.buf[3]-0x5F) + "\t" + String(knockV/1000.0);
+      }
+
+      // direct injections
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x07) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x42) && (msg.buf[3] == 0x67)) {
+        uint32_t directInjections = (msg.buf[4] << 24) + (msg.buf[5] << 16) + (msg.buf[6] << 8) + msg.buf[7];
+        if (directInjections > lastDirectInjections) {
+          str = str + "directInjections:\t" + String(directInjections);
+          lastDirectInjections = directInjections;
+          currentInjectors = "[FSI]";
+        }
+      }
+
+      // MPI injections
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x07) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x42) && (msg.buf[3] == 0x68)) {
+        uint32_t mpiInjections = (msg.buf[4] << 24) + (msg.buf[5] << 16) + (msg.buf[6] << 8) + msg.buf[7];
+        if (mpiInjections > lastMPIInjections) {
+          str = str + "MPIInjections:\t" + String(mpiInjections);
+          lastMPIInjections = mpiInjections;
+          currentInjectors = "[MPI]";
+        }
+      }
+
+      // fuel low: specified
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x29) && (msg.buf[3] == 0x32)) {
+        int pressure = msg.buf[4]*256+msg.buf[5];
+        str = str + "fuel low - spec \t" + String(pressure/1000.0);
+      }
+
+      // fuel low: actual
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x20) && (msg.buf[3] == 0x25)) {
+        int pressure = msg.buf[4]*256+msg.buf[5];
+        str = str + "fuel low - actual \t" + String(pressure/1000.0);
+      }
+
+      // fuel high: specified
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x29) && (msg.buf[3] == 0x3B)) {
+        int pressure = msg.buf[4]*256+msg.buf[5];
+        str = str + "fuel high - specified \t" + String(pressure/10.0);
+      }
+
+      // fuel high: actual
+      else if ((msg.id == 0x7E8) && (msg.buf[0] == 0x05) && (msg.buf[1] == 0x62) && (msg.buf[2] == 0x20) && (msg.buf[3] == 0x27)) {
+        int pressure = msg.buf[4]*256+msg.buf[5];
+        str = str + "fuel high - actual \t" + String(pressure/10.0);
+      }
+
+      if (str!="") {
+        unsigned long allSeconds=time/1000;
+        int runHours= allSeconds/3600;
+        int secsRemaining=allSeconds%3600;
+        int runMinutes=secsRemaining/60;
+        int runSeconds=secsRemaining%60;
+        str = String(time) + "\t" + String(runHours) + ":" + String(runMinutes) + ":" + String(runSeconds) + "\t" + String(lastRPM) + "\t" + currentInjectors + "\t" + str;
+        //Serial.println(str);
+        if (dataFile) {
+          dataFile.println(str);
+          dataFile.flush();
+        }
+      }
+      msg.timeout = 0;
+    }
+  }
+}
